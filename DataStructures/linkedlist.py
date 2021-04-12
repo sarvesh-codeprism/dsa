ad181a1c39a1067c76e7b91b0641a70c58d950c5
Sarvesh Patil <sarveshspatil111@gmail.com>
2021-04-13 02:55:05 +0530

    Update linkedlist.py and add functions print_nth_from_last(), count_occurences_iterative() and count_occurences_recursive()

--- DataStructures/linkedlist.py
+++ DataStructures/linkedlist.py
@@ -193,3 +193,55 @@ class LinkedList:
                 duplicates[curr_node.data] = 1
                 prev_node = curr_node
             curr_node = prev_node.next
+
+    def print_nth_from_last(self, n, method):
+        if method == 1:
+            # Method 1:
+            total_len = self.len_iterative()
+            cur = self.head
+            while cur:
+                if total_len == n:
+                    # print(cur.data)
+                    return cur.data
+                total_len -= 1
+                cur = cur.next
+            if cur is None:
+                return
+
+        elif method == 2:
+            # Method 2:
+            p = self.head
+            q = self.head
+
+            count = 0
+            while q:
+                count += 1
+                if(count >= n):
+                    break
+                q = q.next
+
+            if not q:
+                print(str(n) + " is greater than the number of nodes in list.")
+                return
+
+            while p and q.next:
+                p = p.next
+                q = q.next
+            return p.data
+
+    def count_occurences_iterative(self, data):
+        count = 0
+        cur = self.head
+        while cur:
+            if cur.data == data:
+                count += 1
+            cur = cur.next
+        return count
+
+    def count_occurences_recursive(self, node, data):
+        if not node:
+            return 0
+        if node.data == data:
+            return 1 + self.count_occurences_recursive(node.next, data)
+        else:
+            return self.count_occurences_recursive(node.next, data)
