345e9a6457e8c8b4538833cea68548f94a32540f
Sarvesh Patil <sarveshspatil111@gmail.com>
2021-04-16 11:17:02 +0530

    Update linkedlist.py and add function rotate()

--- DataStructures/linkedlist.py
+++ DataStructures/linkedlist.py
@@ -245,3 +245,26 @@ class LinkedList:
             return 1 + self.count_occurences_recursive(node.next, data)
         else:
             return self.count_occurences_recursive(node.next, data)
+
+    def rotate(self, k):
+        if self.head and self.head.next:
+            p = self.head
+            q = self.head
+            prev = 0
+            count = 0
+
+            while p and count < k:
+                prev = p
+                p = p.next
+                q = q.next
+                count += 1
+            p = prev
+
+            while q:
+                prev = q
+                q = q.next
+            q = prev
+
+            q.next = self.head
+            self.head = p.next
+            p.next = None
