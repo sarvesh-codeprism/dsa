46758903c14e59bbe8ad7779959ba07d796867ec
Sarvesh Patil <sarvesh.patil@codeprism.in>
2022-05-27 08:35:39 +0530

    Add isPowerOfTwo.js

---
+++ Algorithms/js-algo/math/isPowerOfTwo.js
@@ -0,0 +1,29 @@
+function isPowerOfTwo(n) {
+  if (n < 1) {
+    return false;
+  }
+  while (n > 1) {
+    if (n % 2 !== 0) {
+      return false;
+    }
+    n = n / 2;
+  }
+  return true;
+}
+
+console.log(isPowerOfTwo(1)); // true
+console.log(isPowerOfTwo(2)); // true
+console.log(isPowerOfTwo(5)); // false
+// Bitg-O = O(logn)
+
+function isPowerOfTwoBitwise(n) {
+  if (n < 1) {
+    return false;
+  }
+  return (n & (n - 1)) === 0;
+}
+
+console.log(isPowerOfTwoBitwise(1)); // true
+console.log(isPowerOfTwoBitwise(2)); // true
+console.log(isPowerOfTwoBitwise(5)); // false
+// Bitg-O = O(1)
