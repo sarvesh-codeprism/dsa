688aeaaa5e4f27b99018a0112a0fd0c4131c24d3
Sarvesh Patil <sarvesh.patil@codeprism.in>
2022-05-27 08:48:38 +0530

    Update fibonacciSequence.js

--- Algorithms/js-algo/math/fibonacciSequence.js
+++ Algorithms/js-algo/math/fibonacciSequence.js
@@ -1,3 +1,4 @@
+// Iterative : Finding sequennce
 function fibonacci(num) {
   const fib = [0, 1];
   for (let i = 2; i < num; i++) {
@@ -9,5 +10,17 @@ function fibonacci(num) {
 console.log(fibonacci(2)); // [ 0, 1 ]
 console.log(fibonacci(4)); // [ 0, 1, 1, 2 ]
 console.log(fibonacci(6)); // [ 0, 1, 1, 2, 3, 5 ]
+
+// Recursive : Finding nth fibonacci number
+function fibonacciRecursive(num) {
+  if (num === 0 || num === 1) {
+    return num;
+  }
+  return fibonacciRecursive(num - 1) + fibonacciRecursive(num - 2);
+}
+
+console.log(fibonacciRecursive(2));
+console.log(fibonacciRecursive(4));
+console.log(fibonacciRecursive(6));
 
 // Big-O = O(n)
